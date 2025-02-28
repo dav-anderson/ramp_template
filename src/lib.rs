747a0dfbbd305ec8a8cb7257521e842043babd2e
dav-anderson <david@orange.me>
2025-02-28 12:42:16 -0500

    update rust on rails

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
-use rust_on_rails2::*;
+// use rust_on_rails::*;
 
-pub struct MyApp {}
+// pub struct MyApp {}
+
+// impl App for MyApp {
+//     const LOG_LEVEL: LogLevel = LogLevel::Info;
+
+//     async fn new() -> Self {
+//     //  let fonts = HashMap::from([("bold".to_string(), include_bytes!("../assets/outfit_bold.ttf").to_vec())]);
+//         MyApp{}
+//     }
+
+//     async fn draw(&mut self) -> Vec<Mesh> {
+//         vec![Mesh{
+//             shape: Shape::Rectangle(100, 100),
+//             offset: (100, 100)
+//         }]
+//     }
+// }
+
+// create_entry_points!(CanvasApp::<MyApp>);
+
+
+use rust_on_rails::prelude::*;
+pub struct MyApp;
 
 impl App for MyApp {
-    const LOG_LEVEL: LogLevel = LogLevel::Info;
+    async fn new(ctx: &mut Context<'_>) -> Box<dyn ComponentBuilder> {
+        ctx.include_assets(include_assets!("./assets"));
+        println!("my app");
 
-    async fn new() -> Self {
-    //  let fonts = HashMap::from([("bold".to_string(), include_bytes!("../assets/outfit_bold.ttf").to_vec())]);
-        MyApp{}
-    }
+        Box::new(Shape(ShapeType::Rectangle(50,50),"ffffff",None))
 
-    async fn draw(&mut self) -> Vec<Mesh> {
-        vec![Mesh{
-            shape: Shape::Rectangle(100, 100),
-            offset: (100, 100)
-        }]
     }
 }
 
-create_entry_points!(CanvasApp::<MyApp>);
+create_entry_points!(MyApp);
+
+
