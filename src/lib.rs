fc9758f3c7efc2e57e305fe65ba1a724b3d89f6b
dav-anderson <david@orange.me>
2025-02-28 12:42:48 -0500

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,3 @@
-// use rust_on_rails::*;
-
-// pub struct MyApp {}
-
-// impl App for MyApp {
-//     const LOG_LEVEL: LogLevel = LogLevel::Info;
-
-//     async fn new() -> Self {
-//     //  let fonts = HashMap::from([("bold".to_string(), include_bytes!("../assets/outfit_bold.ttf").to_vec())]);
-//         MyApp{}
-//     }
-
-//     async fn draw(&mut self) -> Vec<Mesh> {
-//         vec![Mesh{
-//             shape: Shape::Rectangle(100, 100),
-//             offset: (100, 100)
-//         }]
-//     }
-// }
-
-// create_entry_points!(CanvasApp::<MyApp>);
-
-
 use rust_on_rails::prelude::*;
 pub struct MyApp;
 
