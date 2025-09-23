860d8d2ea59203d6eb15f46b9cf9408255e50bed
dav-anderson <david@orange.me>
2025-09-23 10:56:42 -0400

    updates to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,83 @@
-use pelican_ui::{AndroidApp, Component, Context, Plugins, Plugin, start, Application};
+use pelican_ui::{Component, Context, Plugins, Plugin, start, Application};
 use pelican_ui::drawable::{Drawable, Component, Align};
 use pelican_ui::layout::{Layout, SizeRequest, Area};
 use pelican_ui::events::OnEvent;
 use std::collections::BTreeMap;
+use pelican_ui_std::AppPage;
+use pelican_ui_std::components::interface::general::{Interface, Page, Content, Header};
+use pelican_ui_std::layout::{Stack, Offset};
+use pelican_ui_std::components::{Text, TextStyle, Icon, ExpandableText};
 
-use pelican_ui_std::{
-    Interface, Stack, 
-    Page, Text, TextStyle,
-    Offset, Content, Icon,
-    ExpandableText, Header,
-    AppPage,
-};
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "PhotosUI", kind = "framework")]
+extern "C" {}
+
+#[cfg(target_os = "macos")]
+#[link(name = "Cocoa", kind = "framework")]
+extern "C" {}
+
+#[cfg(target_os = "macos")]
+#[link(name = "ApplicationServices", kind = "framework")]
+extern "C" {}
+
+#[cfg(target_os = "macos")]
+#[link(name = "AppKit", kind = "framework")]
+extern "C" {}
+
+#[cfg(target_os = "macos")]
+#[link(name = "Carbon", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "main", kind = "static")]
+extern "C" {}
+
+#[cfg(target_os = "ios")]
+#[link(name = "UIKit", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "CoreGraphics", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "Metal", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "CoreVideo", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "CoreMedia", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "AVKit", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "AVFoundation", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]#[link(name = "Security", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "QuartzCore", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "c++")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "AudioToolbox", kind = "framework")]
+extern "C" {}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
+#[link(name = "Foundation", kind = "framework")]
+extern "C" {}
 
 // Define the main application struct. This is our entry point type.
 pub struct MyApp;
